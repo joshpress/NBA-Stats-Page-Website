134905f77534013b8c2330d2d7cacc7014342778
joshpress <joshbunny2004@yahoo.com>
2026-04-24 10:52:08 -0700

    Add new files

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 document.addEventListener("DOMContentLoaded",DOMContentLoaded);
+const NBA = require("nba");
+
 
 function DOMContentLoaded() {
     let submitPlayer=document.getElementById("submitPlayer");
     let submitTeam=document.getElementById("submitTeam");
-    submitPlayer.addEventListener("click",submitPlayerClick);
-    submitTeam.addEventListener("click",submitTeamClick);
+    //submitPlayer.addEventListener("click",submitPlayerClick);
+     getJSON();
+  //  submitTeam.addEventListener("click",submitTeamClick);
 }
 
 function submitPlayerClick() {
     let playerName=document.getElementById("playerName").value;
     let numberofGames=document.getElementById("numberofGames").value;
+   
 }
 function submitTeamClick() {
+}
+
+async function getJSON(){
+    const player = NBA.findPlayer('Stephen Curry');
+    NBA.stats.playerInfo({ PlayerID: player.playerId }).then(console.log);
+    try{
+        
+        const response= await fetch ("json/team_statistics.json");
+        const data= await response.json();
+
+        const team=data.find(p=>p.teamName==="Warriors");
+
+        console.log(player);
+        return player;
+    
+    }
+    catch(error){
+        console.log(`Error ${error}`)
+    }
 }
