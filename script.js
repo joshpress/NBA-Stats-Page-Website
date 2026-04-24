4886ff5dd8bb0ffab8d338bff254f6625cb2e334
joshpress <joshbunny2004@yahoo.com>
2026-04-24 11:28:49 -0700

    Update JS

--- script.js
+++ script.js
@@ -1,38 +1,76 @@
+let nbaData=[];
+let playerData=[]
 document.addEventListener("DOMContentLoaded",DOMContentLoaded);
-const NBA = require("nba");
 
+ 
 
 function DOMContentLoaded() {
-    let submitPlayer=document.getElementById("submitPlayer");
-    let submitTeam=document.getElementById("submitTeam");
-    //submitPlayer.addEventListener("click",submitPlayerClick);
-     getJSON();
-  //  submitTeam.addEventListener("click",submitTeamClick);
-}
-
-function submitPlayerClick() {
-    let playerName=document.getElementById("playerName").value;
-    let numberofGames=document.getElementById("numberofGames").value;
    
-}
-function submitTeamClick() {
-}
-
-async function getJSON(){
-    const player = NBA.findPlayer('Stephen Curry');
-    NBA.stats.playerInfo({ PlayerID: player.playerId }).then(console.log);
-    try{
-        
-        const response= await fetch ("json/team_statistics.json");
-        const data= await response.json();
-
-        const team=data.find(p=>p.teamName==="Warriors");
-
-        console.log(player);
-        return player;
     
+
+    submitPlayer.addEventListener("click",submitPlayerClick);
+   
+     loadData();
+
+    submitPlayer.addEventListener("click",submitPlayerClick);
+  
+}
+async function loadData(){
+    try{
+        const response= await fetch("json/nba_games_2004_2026.json");
+        nbaGamesData=await response.json();
+
     }
     catch(error){
-        console.log(`Error ${error}`)
+        console.log(`Error ${error}`);
     }
 }
+function submitPlayerClick() {
+    let searchInput=document.getElementById("searchInput").value;
+    console.log(searchInput.value)
+    search(searchInput);
+   
+}
+
+
+function search(term){
+    const searchTerm=term.toLowerCase();
+    if(!searchTerm){
+        renderTable(filtered.slice(0,50));
+        return;
+    }
+     const filtered = nbaGamesData.filter(game => {
+        return (
+           
+            game.TEAM_NAME?.toLowerCase().includes(searchTerm) ||
+            game.GAME_DATE?.includes(searchTerm)
+        );
+    });
+
+    renderTable(filtered.slice(0,50));
+}
+
+function renderTable(data){
+    const headerRow=document.getElementById("headerRow");
+    const tableBody=document.getElementById("tableBody");
+
+    headerRow.innerHTML="";
+    tableBody.innerHTML="";
+    const columns=["GAME_DATE","TEAM_ABBREVIATION","PTS","REB","AST","WL"]
+    columns.forEach(col=>{
+        const th=document.createElement('th');
+        th.innerText=col.replace("_"," ");
+        headerRow.appendChild(th);
+    })
+
+    data.forEach(game=>{
+        const tr=document.createElement("tr");
+        columns.forEach(col=>{
+            const td=document.createElement("td");
+            td.innerText=game[col]?? "-";
+            tr.appendChild(td);
+        })
+        tableBody.appendChild(tr);
+    })
+}
+
